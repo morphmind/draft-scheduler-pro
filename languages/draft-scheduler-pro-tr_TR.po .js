cd3cdf49e69ecf30fba0b91df4d2b13d93f7396e
morphmind <kaan@yildizkan.com>
2024-11-13 21:32:54 +0300

    Create draft-scheduler-pro-tr_TR.po .js

---
+++ languages/draft-scheduler-pro-tr_TR.po .js
@@ -0,0 +1,60 @@
+msgid ""
+msgstr ""
+"Project-Id-Version: Draft Scheduler Pro\n"
+"POT-Creation-Date: 2024-03-15\n"
+"PO-Revision-Date: 2024-03-15\n"
+"Last-Translator: \n"
+"Language-Team: Turkish\n"
+"Language: tr_TR\n"
+"MIME-Version: 1.0\n"
+"Content-Type: text/plain; charset=UTF-8\n"
+"Content-Transfer-Encoding: 8bit\n"
+"Plural-Forms: nplurals=2; plural=(n != 1);\n"
+
+msgid "Draft Scheduler Pro"
+msgstr "Taslak Zamanlayıcı Pro"
+
+msgid "Schedule your draft posts with advanced timing options. Choose between sequential, random, or custom scheduling patterns."
+msgstr "Taslak gönderilerinizi gelişmiş zamanlama seçenekleriyle planlayın. Sıralı, rastgele veya özel zamanlama düzenleri arasından seçim yapın."
+
+msgid "Basic Settings"
+msgstr "Temel Ayarlar"
+
+msgid "Post Type"
+msgstr "Gönderi Türü"
+
+msgid "Categories (Optional)"
+msgstr "Kategoriler (İsteğe Bağlı)"
+
+msgid "Start Date"
+msgstr "Başlangıç Tarihi"
+
+msgid "Schedule Type"
+msgstr "Zamanlama Türü"
+
+msgid "Sequential"
+msgstr "Sıralı"
+
+msgid "Schedule posts in order from oldest to newest"
+msgstr "Gönderileri eskiden yeniye doğru sırayla zamanla"
+
+msgid "Random"
+msgstr "Rastgele"
+
+msgid "Schedule posts in random order"
+msgstr "Gönderileri rastgele sırayla zamanla"
+
+msgid "Custom Time Window"
+msgstr "Özel Zaman Aralığı"
+
+msgid "Schedule posts within specific time windows"
+msgstr "Gönderileri belirli zaman aralıklarında zamanla"
+
+msgid "Schedule Preview"
+msgstr "Zamanlama Önizlemesi"
+
+msgid "Undo Last Schedule"
+msgstr "Son Zamanlamayı Geri Al"
+
+msgid "Schedule Drafts"
+msgstr "Taslakları Zamanla"
